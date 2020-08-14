88b431409b6d4ef8232a091c10ab4c244f463508
Will Blair <wdblair@ibm.com>
2020-08-14 16:31:04 +0000

    Check in CGC experimental set up.

---
+++ experiments/redhat/cgc/cgc.mk
@@ -0,0 +1,9 @@
+all: ${AUTHOR_ID}_${SERVICE_ID}
+
+OBJECTS := $(patsubst %.c,%.o,$(wildcard lib/*.c src/*.c))
+
+%.o: %.c
+	gcc -I /cgc/ -I ${PWD} -I ${PWD}/lib ${CFLAGS} -c -o $@ $<
+
+${AUTHOR_ID}_${SERVICE_ID}: ${OBJECTS} /cgc/libcgc.o
+	gcc -o $@ $^
---
+++ experiments/redhat/cgc/cgc.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+/cgc/challenges/$1/$1
