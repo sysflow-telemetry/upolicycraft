a5114c9c2406d2e1e0aacb7977e6801690e796ac
William Blair <wdblair@ibm.com>
2020-12-29 14:16:59 -0500

    Check in missing files

---
+++ plugins/mrm/util.go
@@ -0,0 +1,19 @@
+//
+// Copyright (C) 2020 IBM Corporation.
+//
+// Authors:
+// William Blair <wdblair@ibm.com>
+// Frederico Araujo <frederico.araujo@ibm.com>
+// Teryl Taylor <terylt@ibm.com>
+//
+package main
+
+// Contains determines whether a value appears in a slice.
+func Contains(slice []string, val string) bool {
+	for _, item := range slice {
+		if item == val {
+			return true
+		}
+	}
+	return false
+}
