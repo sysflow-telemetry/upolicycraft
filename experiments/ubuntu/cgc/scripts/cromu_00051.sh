1b7b4c27d5205512fed1918d7848069e921ad907
Will Blair <wdblair@ibm.com>
2021-01-25 07:52:49 +0000

    Add scripts for modelling with xinetd

---
+++ experiments/ubuntu/cgc/scripts/cromu_00051.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+/uIDS/uIDS/bin/run /uIDS/tests/cgc/CROMU_00051 _start "/cgc/CROMU_00051" /usr/sbin/xinetd -dontfork greedy "%000000" 200000000 
+
---
+++ experiments/ubuntu/cgc/scripts/cromu_00055.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+/uIDS/uIDS/bin/run /uIDS/tests/cgc/CROMU_00055 _start "/cgc/CROMU_00055" /usr/sbin/xinetd -dontfork greedy "%000000" 200000000 
+
